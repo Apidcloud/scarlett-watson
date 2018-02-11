8c9fa360a69b051b4574a57c4e30d7c2ace722e4
Apidcloud <luisapidcloud@hotmail.com>
2018-02-11 18:10:41 +0000

    feat: add working watson speech to text example

---
+++ public/client.js
@@ -0,0 +1,48 @@
+'use strict';
+// notes:
+//
+// * This file is bundled by webpack-dev-middleware into bundle.js
+//
+// * The require('watson-developer-cloud/language_translator/v2') could also be written as require('watson-developer-cloud').LanguageTranslatorV2,
+//   but that version results in a much larger bundle size.
+//
+// * Tokens expire after 1 hour. This demo simply fetches a new one for each translation rather than keeping a fresh one.
+//
+// * fetch() is a modern version of XMLHttpRequest. A pollyfill is available for older browsers: https://github.com/github/fetch
+
+var recognizeMic = require('watson-speech/speech-to-text/recognize-microphone');
+
+var btn = document.getElementById('analyze-btn');
+var input = document.getElementById('input');
+var output = document.getElementById('output');
+
+/**
+ * @return {Promise<String>} returns a promise that resolves to a string token
+ */
+function getToken() {
+  return fetch('http://localhost:3000/api/speech-to-text/token').then(function(response) {
+    return response.text();
+  }); 
+}
+
+function recognizeSpeech(token) {
+  var stream = recognizeMic({
+    token: token,
+    objectMode: true, // send objects instead of text
+    extractResults: true, // convert {results: [{alternatives:[...]}], result_index: 0} to {alternatives: [...], index: 0}
+    format: false // optional - performs basic formatting on the results such as capitals an periods
+  });
+  stream.on('data', (data) => {
+    output.innerHTML = data.alternatives[0].transcript
+  });
+  stream.on('error', function(err) {
+    if (err) {
+      output.innerHTML = err;
+      return console.log(err);
+    }
+  });
+}
+
+btn.onclick = function() {
+  getToken().then(recognizeSpeech);
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+var express = require('express'); // eslint-disable-line node/no-missing-require
+var app = express();
+var watson = require('watson-developer-cloud');
+
+// bundle the code
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config');
+
+var compiler = webpack(webpackConfig);
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/' // Same as `output.publicPath` in most cases.
+  })
+);
+
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  res.header("Content-Security-Policy", "font-src 'self' data:")
+  next();
+});
+
+// For local development, specify the username and password or set env properties
+var ltAuthService = new watson.AuthorizationV1({
+  username: process.env.SPEECH_TO_TEXT_USERNAME || '<username>',
+  password: process.env.SPEECH_TO_TEXT_PASSWORD || '<password>',
+  url: watson.SpeechToTextV1.URL
+});
+
+app.use('/api/speech-to-text/token', function(req, res) {
+  ltAuthService.getToken(function(err, token) {
+    if (err) {
+      console.log('Error retrieving token: ', err);
+      return res.status(500).send('Error retrieving token');
+    }
+    res.send(token);
+  });
+});
+
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+app.listen(port, function() {
+  console.log('Watson webpack example server running at http://localhost:%s/', port);
+});
